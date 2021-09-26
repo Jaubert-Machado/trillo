3d061f0592b8047584125c9afafb3e2576dd34e1
Jaubert Machado <80221925+Jaubert-Machado@users.noreply.github.com>
2021-09-26 19:18:18 -0300

    hiddin dropdown menu

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const userNavMenu = document.querySelector(".user-nav__menu");
 const userNavUser = document.querySelector(".user-nav__user");
 
-userNavUser.addEventListener("click", function () {
-  userNavMenu.classList.toggle("show-menu");
-});
+// userNavUser.addEventListener("click", function () {
+//  userNavMenu.classList.toggle("show-menu");
+// });
